4d04384519f59f92885a7a9b6e07113177b43368
september21st <tsuilook@gmail.com>
2022-02-02 23:37:52 +0900

    :sparkles: 사진검색 api 작성

--- 무한스크롤/src/App.js
+++ 무한스크롤/src/App.js
@@ -1,7 +1,16 @@
+import { request } from './api.js'
 import Header from './components/Header.js'
 import Input from './components/Input.js'
 
 export default function App($target) {
   new Header({ $target })
   new Input({ $target })
+
+  const searchPhotos = async (query, page = 1) => {
+    try {
+      const data = await request(`/search/photos?query=${query}&page=${page}`)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 }

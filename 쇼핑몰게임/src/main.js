8e4697faa5b71e5a1efc08bec3df1d8c7878a577
september21st <tsuilook@gmail.com>
2022-02-18 00:17:28 +0900

    :sparkles: 모든 아이템 가져오기

--- 쇼핑몰게임/src/main.js
+++ 쇼핑몰게임/src/main.js
@@ -1,1 +1,21 @@
-console.log('app is running')
+const $itemList = document.querySelector('.item-list')
+
+async function fetchItems() {
+  const response = await fetch('../data/data.json')
+  return response.json()
+}
+
+const renderItem = (image, gender, size) => {
+  return ` <li class='item'><img src="../public/images/${image}" class="item-image" /><span class="item-tag">${gender}</span><span class="item-tag">${size}</span></li>`
+}
+
+const getAllItems = async () => {
+  const { items } = await fetchItems()
+  console.log(items)
+
+  $itemList.innerHTML = `
+    ${items.map((item) => renderItem(item.image, item.gender, item.size)).join('')}
+  `
+}
+
+getAllItems()

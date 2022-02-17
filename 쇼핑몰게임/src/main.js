f9175342bb28e9d2b47c7a0046f1b5bebe7fe9a1
september21st <tsuilook@gmail.com>
2022-02-18 00:44:16 +0900

    :sparkles: 컬러버튼 출력시 필터링된 리스트 출력

--- 쇼핑몰게임/src/main.js
+++ 쇼핑몰게임/src/main.js
@@ -1,5 +1,6 @@
 const $itemList = document.querySelector('.item-list')
 const $imageButton = document.querySelectorAll('.img-btn')
+const $colorButton = document.querySelectorAll('.btn.color')
 
 async function fetchItems() {
   const response = await fetch('../data/data.json')
@@ -31,6 +32,24 @@ $imageButton.forEach((button) =>
       filtered = items.filter((item) => item.type === 'pants')
     } else {
       filtered = items.filter((item) => item.type === 'skirt')
+    }
+
+    $itemList.innerHTML = `
+    ${filtered.map((item) => renderItem(item.image, item.gender, item.size)).join('')}
+  `
+  })
+)
+
+$colorButton.forEach((button) =>
+  button.addEventListener('click', async () => {
+    const { items } = await fetchItems()
+    let filtered
+    if (button.dataset.value === 'blue') {
+      filtered = items.filter((item) => item.color === 'blue')
+    } else if (button.dataset.value === 'pink') {
+      filtered = items.filter((item) => item.color === 'pink')
+    } else {
+      filtered = items.filter((item) => item.color === 'yellow')
     }
 
     $itemList.innerHTML = `

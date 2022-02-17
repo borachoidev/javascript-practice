c0b3f64bf65304156ff6fc65e9a8d7dd839700be
september21st <tsuilook@gmail.com>
2022-02-17 21:45:43 +0900

    :necktie: 검색어에 따른 api요청

--- 무한스크롤/src/App.js
+++ 무한스크롤/src/App.js
@@ -2,13 +2,31 @@ import { request } from './api.js'
 import Header from './components/Header.js'
 import Input from './components/Input.js'
 
-export default function App($target) {
+export default function App($target, initialState) {
+  this.state = initialState
+
+  this.setState = (nextState) => {
+    this.state = nextState
+  }
+
   new Header({ $target })
-  new Input({ $target })
+  new Input({
+    $target,
+    initialState: {
+      query: '',
+      page: 1,
+    },
+    onSearch: async (query, page) => {
+      const data = await searchPhotos(query, page)
+      this.setState({ ...this.state, data: [...data.results] })
+      console.log(data)
+    },
+  })
 
   const searchPhotos = async (query, page = 1) => {
     try {
       const data = await request(`/search/photos?query=${query}&page=${page}`)
+      return data
     } catch (error) {
       console.error(error)
     }

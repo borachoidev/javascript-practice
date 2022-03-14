0aa43ca783a471f9062b16f4f4fd0d7cdf99cbff
september21st <tsuilook@gmail.com>
2022-03-14 16:06:16 +0900

    :sparkles: 고양이 검색 결과 출력

--- 고양이검색사이트/src/app.js
+++ 고양이검색사이트/src/app.js
@@ -1,11 +1,33 @@
+import { api } from './api/index.js'
 import Headers from './components/header.js'
+import SearchResult from './components/searchResult.js'
 
 console.log('app')
 export default function App(target) {
-  this.state = {}
+  this.state = {
+    result: [],
+  }
 
-  new Headers({ target })
+  new Headers({
+    target,
+    onSearch: async query => {
+      try {
+        const { data } = await api.searchCats(query)
+        this.setState({ ...this.state, result: data })
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+  })
+
+  const searchResult = new SearchResult({
+    target,
+    initialState: this.state.result,
+  })
+
   this.setState = nextState => {
     this.state = nextState
+    searchResult.setState(this.state.result)
   }
 }

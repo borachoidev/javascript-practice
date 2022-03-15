c8e776b1ae8948e2a7c4ce1177efd3e570259872
september21st <tsuilook@gmail.com>
2022-03-15 16:24:58 +0900

    :sparkles: 최근 검색어 기능 추가

--- 고양이검색사이트/src/components/recentQuery.js
+++ 고양이검색사이트/src/components/recentQuery.js
@@ -1,5 +1,6 @@
-export default function RecentQuery({ target, initialState }) {
+export default function RecentQuery({ target, initialState, onSearch }) {
   this.state = initialState
+  this.onSearch = onSearch
 
   const recentQuery = document.createElement('section')
   recentQuery.classList = 'recent-query'
@@ -13,13 +14,25 @@ export default function RecentQuery({ target, initialState }) {
 
   this.render = () => {
     recentQuery.innerHTML = `
+    <p>최근 검색어 </p>
     <ul class="recent-query__list">
-      <li class="recent-query__item">검색어</li>
-      <li class="recent-query__item">검색어</li>
-      <li class="recent-query__item">검색어</li>
+    ${this.state
+      .map(
+        query =>
+          `<li class="recent-query__item" data-query="${query}">${query}</li>`
+      )
+      .join('')}
+  
     </ul>
     `
   }
+
+  recentQuery.addEventListener('click', event => {
+    const item = event.target.closest('.recent-query__item')
+    if (!item) return
+    const { query } = item.dataset
+    this.onSearch(query)
+  })
 
   this.render()
 }
